d4a28281842f006df788744fb30e12acce7606e0
jasoonkp <jasonprakash1214@gmail.com>
2025-09-17 01:57:33 -0700

    final implemntation, quicksort works great

--- main.py
+++ main.py
@@ -4,29 +4,48 @@ from random import randint
 # defines array
 numArray = [0]*25
 
-# generates random array
 
+# generates random array
 for i in range(0,25):
     numArray[i] = randint(1,100)
 
-#prints array
+
+# swap 2 values in array method
+# parameters are array, and indices used to swap elements
+def swapValues(numArray,i,j):
+    numArray[i], numArray[j] = numArray[j], numArray[i]
+
+
+# prints array method
 def printArray(numArray):
     r = len(numArray) 
     for i in range(0,r):
-        print(numArray[i])
+        print(numArray[i], end = " ")
+    
+    print("")
 
+
+# partioning method
+# var i is greater than pivot, var j is less than pivot
 def Partition(numArray,l,r):
-    m = randint(l,r)
-    numArray[m], numArray[r] = numArray[r], numArray[m]
-    print("test")
+    i = l - 1
+
+    pivot = randint(l,r)
+    swapValues(numArray,pivot,r)
+    for j in range(l,r):
+        if numArray[j] <= numArray[r]:
+            swapValues(numArray,j,i+1)
+            i += 1
+    swapValues(numArray, r,i+1)
+    return i + 1
 
 
+# quicksort method (recursive)
 def Quicksort(numArray,l,r):
-    print(numArray[0])
+    printArray(numArray)
+    print("")
 
-    r = len(numArray)
-    l = 0
-    if r <= l:
+    if l >= r:
         return
     q = Partition(numArray,l,r)
     Quicksort(numArray,l,q-1)
@@ -38,6 +57,9 @@ def Quicksort(numArray,l,r):
 
 
 def main():
+    printArray(numArray)
+    swapValues(numArray, 1 ,2)
+    Quicksort(numArray,0,len(numArray)-1)
     printArray(numArray)
 
 if __name__ == "__main__":
